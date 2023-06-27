c0aaf5e9a71cc77d70b195faecd0782edadffbbb
Marcos Gonçalves <marcos.goncalves@avanade.com>
2023-06-26 22:11:35 -0300

    Closes #7: Controller for Product

---
+++ src/main/java/com/darksun/controller/ProductController.java
@@ -0,0 +1,43 @@
+package com.darksun.controller;
+
+import com.darksun.model.Product;
+import com.darksun.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping( "/products" )
+public class ProductController {
+	@Autowired
+	ProductService service;
+
+	@PostMapping
+	public ResponseEntity< Product > create( @RequestBody Product product ) {
+		return new ResponseEntity<>( service.create( product ), HttpStatus.CREATED );
+	}
+
+	@GetMapping
+	public ResponseEntity< List< Product > > readAll( ) {
+		return new ResponseEntity<>( service.readAll( ), HttpStatus.OK );
+	}
+
+	@GetMapping( "/{id}" )
+	public ResponseEntity< Product > readById( @PathVariable Long id ) {
+		return new ResponseEntity<>( service.readById( id ), HttpStatus.OK );
+	}
+
+	@PutMapping
+	public ResponseEntity< Product > update( @RequestBody Product product ) {
+		return new ResponseEntity<>( service.update( product ), HttpStatus.OK );
+	}
+
+	@DeleteMapping( "/{id}" )
+	public ResponseEntity delete( @PathVariable Long id ) {
+		service.delete( id );
+		return new ResponseEntity( HttpStatus.NO_CONTENT );
+	}
+}
